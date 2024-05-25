71e38c4cc604f3e41885bd5eeb598897e34e1577
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-05-24 21:34:01 -0300

    Added Injectable decorator to mark the class as available to be provided and injected as a dependency.

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,5 +1,9 @@
+import { Injectable } from '@angular/core';
 import { data, User } from './data';
 
+@Injectable({
+  providedIn: 'root', //Available for the whole application
+})
 export class UserService {
   private userData: User[] = data;
 
