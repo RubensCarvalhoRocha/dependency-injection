c353383f141d154408eb94cb75a1ba8420fdbee9
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-05-24 21:33:01 -0300

    added instance of userService (inject) to get User Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from './user.service';
+import { User } from './data';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <h1>User Listing</h1> `,
+  imports: [RouterOutlet, UserInfoComponent],
+  template: ` <h1>User Listing</h1>
+  @for (user of userData; track user.id) {
+    <app-user-info [user]="user"/>
+  }
+  `,
 })
 export class AppComponent {
-  title = 'dependency-injection';
+  userService = inject(UserService);
+  userData: User[] = [];
+
+  constructor() {
+    this.userService.getUserData().then((data) => {
+      this.userData = data;
+    });
+  }
 }
